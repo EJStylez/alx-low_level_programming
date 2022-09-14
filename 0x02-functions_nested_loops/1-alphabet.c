befde92aa52b7d27fe8a489777bd6a36b38dfcf4
EJStylez <enochiworldwide@gmail.com>
2022-09-13 19:18:33 -0700

    working on variables and functions of c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - function to print abc
+ *
  * Return: 0
  */
-
 void print_alphabet(void)
 {
-	char c = 'a';	
-	while (c <= 'z')
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
-		c++;
 	}
 	_putchar('\n');
 }
